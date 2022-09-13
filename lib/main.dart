cbe761bc00b071d221b7880388e2554c7df2e061
Rishikesh-kumar-7258 <rishi7258prince@gmail.com>
2022-09-13 19:51:26 +0530

    creating a new version

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:world_time/pages/choose_location.dart';
-import "package:world_time/pages/home.dart";
 import 'package:world_time/pages/loading.dart';
 
 void main() {
@@ -8,8 +6,6 @@ void main() {
     initialRoute: '/',
     routes: {
       '/': (context) => const Loading(),
-      '/home': (context) => const Home(),
-      '/location': (context) => const ChooseLocation()
     },
   ));
 }
--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:world_time/services/world_time.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -10,34 +8,8 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  Future<void> setupWorldTime() async {
-    WorldTime instance =
-        WorldTime(flag: 'india.png', location: "kolkata", url: "asia/kolkata");
-
-    await instance.getTime();
-
-    // Navigator.pushReplacementNamed(context, "/home", arguments: {
-    //   'location': instance.location,
-    //   'time': instance.time,
-    //   'flag': instance.flag
-    // });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    setupWorldTime();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.blue[900],
-        body: const Center(
-          child: SpinKitRotatingCircle(
-            color: Colors.white,
-            size: 50.0,
-          ),
-        ));
+    return Container();
   }
 }
